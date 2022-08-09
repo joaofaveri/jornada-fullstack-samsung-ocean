69121c4e67cb3f36b76f42f8e61832a900050c60
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-08-09 15:46:50 +0000

    feat: create a new get route

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,12 @@ import express from "express";
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello, world! Tanks, Nodemon!");
+  res.send("Hello, world!");
+});
+
+// Criar uma nova rota GET
+app.get("/oi", function (req, res) {
+  res.send("Olá, Mundo!");
 });
 
 app.listen(3000);
