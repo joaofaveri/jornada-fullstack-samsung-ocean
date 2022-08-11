a393ec73128d79bf87fbdc6312abc716e34c423a
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-08-11 15:24:50 +0000

    feat: scores read all and create endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,53 @@
 import express from 'express'
+
 const app = express()
 
-app.get('/', function (req, res) {
+app.use(express.json())
+
+app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
 // Criar uma nova rota GET
-app.get('/oi', function (req, res) {
+app.get('/oi', (req, res) => {
   res.send('Olá, Mundo!')
 })
 
-app.listen(3000)
+// Lista com as pontuações
+const highScores = [
+  {
+    id: 1,
+    name: 'Fulano',
+    score: 90
+  },
+  {
+    id: 2,
+    name: 'Ciclano',
+    score: 80
+  },
+  {
+    id: 3,
+    name: 'Beltrano',
+    score: 70
+  }
+]
+
+// Endpoints scores:
+// Read All - GET - /scores
+app.get('/scores', (req, res) => {
+  res.json(highScores)
+})
+
+// Create - POST - /scores
+app.post('/scores', (req, res) => {
+  const score = req.body
+  // Inserir o score na Lista
+  highScores.push({
+    id: highScores.length + 1,
+    ...score
+  })
+  console.log(highScores)
+  res.json(highScores)
+})
+
+app.listen(3000, () => console.log(`Server is running in port 3000`))
