adb3ef5e71c3caf4b3e07d92363cd5c5b7549393
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-08-16 14:11:08 +0000

    feat: database connection

--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,70 @@
 import express from 'express'
+import { MongoClient } from 'mongodb'
 
-const app = express()
+const url = 'mongodb://localhost:27017'
+const databaseName = 'jornada-fullstack-agosto-22'
 
-app.use(express.json())
+async function main() {
+  // Realizar a conexão com o MongoClient
+  // MongoClient -> MongoDatabase -> MongoCollection
+  // Conexões podem levar um tempo para concluir
+  // Utilizar Promisses
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+  const client = await MongoClient.connect(url)
+  const database = client.db(databaseName)
+  const collection = database.collection('scores')
 
-// Criar uma nova rota GET
-app.get('/oi', (req, res) => {
-  res.send('Olá, Mundo!')
-})
+  const app = express()
 
-// Lista com as pontuações
-const highScores = [
-  {
-    id: 1,
-    name: 'Fulano',
-    score: 90
-  },
-  {
-    id: 2,
-    name: 'Ciclano',
-    score: 80
-  },
-  {
-    id: 3,
-    name: 'Beltrano',
-    score: 70
-  }
-]
+  app.use(express.json())
 
-// Endpoints scores:
-// Read All - GET - /scores
-app.get('/scores', (req, res) => {
-  res.json(highScores)
-})
-
-// Create - POST - /scores
-app.post('/scores', (req, res) => {
-  const score = req.body
-  // Inserir o score na Lista
-  highScores.push({
-    id: highScores.length + 1,
-    ...score
+  app.get('/', (req, res) => {
+    res.send('Hello, world!')
   })
-  res.json(highScores)
-})
 
-app.listen(3000, () => console.log(`Server is running in port 3000`))
+  // Criar uma nova rota GET
+  app.get('/oi', (req, res) => {
+    res.send('Olá, Mundo!')
+  })
+
+  // Lista com as pontuações
+  const highScores = [
+    {
+      id: 1,
+      name: 'Fulano',
+      score: 90
+    },
+    {
+      id: 2,
+      name: 'Ciclano',
+      score: 80
+    },
+    {
+      id: 3,
+      name: 'Beltrano',
+      score: 70
+    }
+  ]
+
+  // Endpoints scores:
+  // Read All - GET - /scores
+  app.get('/scores', (req, res) => {
+    res.json(highScores)
+  })
+
+  // Create - POST - /scores
+  app.post('/scores', (req, res) => {
+    const score = req.body
+    // Inserir o score na Lista
+    highScores.push({
+      id: highScores.length + 1,
+      ...score
+    })
+    res.json(highScores)
+  })
+
+  app.listen(3000, () => console.log(`Server is running in port 3000`))
+}
+
+// Executar a função main()
+main()
