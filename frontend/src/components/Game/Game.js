48afa58c550d1bd151b51683ad5fbcb3efe35184
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-08-23 13:22:01 +0000

    feat: insert new assets and animated

--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -1,10 +1,19 @@
 import './game.css'
 import clouds from '../../assets/clouds.png'
+import pipeline from '../../assets/pipeline.png'
+import character from '../../assets/mario.gif'
+import ground from '../../assets/ground.png'
 
 const Game = () => {
+  document.onkeydown = () => {
+    console.log('On Key Down')
+  }
   return (
     <div className="game">
       <img className="clouds" src={clouds} alt="Clouds" />
+      <img className="pipeline" src={pipeline} alt="Pipeline" />
+      <img className="character" src={character} alt="Character" />
+      <div className="ground" style={{ backgroundImage:`url(${ground})`,backgroundRepeat:"repeat-x",backgroundSize:"contain" }}></div>
     </div>
   )
 }
