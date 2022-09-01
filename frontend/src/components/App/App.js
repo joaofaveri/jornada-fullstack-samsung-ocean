cf2fecc4ff9df1ecbebdc8e66fe2b3e95d36294d
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-09-01 13:49:38 +0000

    fix: corrige css class name

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -17,7 +17,7 @@ const App = () => {
   }
 
   return (
-    <div className={App}>
+    <div>
       <Game onDead={onDead} onScore={onScore} />
       {gameOver && <HighScore score={score} />}
     </div>
